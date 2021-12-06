e546c81223c2a54df0b3cd9cb5fcf76453799ce0
Cesar Guerrero <cesarguerrero@transfix.io>
2021-12-06 15:36:09 -0500

    sandbox config

--- index.js
+++
@@ -1,7 +0,0 @@
-var ifrm = document.createElement('iframe');
-ifrm.setAttribute('id', 'ifrm'); // assign an id
-
-document.body.appendChild(ifrm); // to place at end of document
-
-// assign url
-ifrm.setAttribute('src', 'https://apps-staging.transfix.io/#/graphiql');
